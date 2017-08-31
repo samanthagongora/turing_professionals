5f7091c202b0f511d4dc48d4ca4df7ec14ef9003
Ellen Cooper <ellencooper74@gmail.com>
2017-08-31 15:50:31 -0600

    adds feature test for viewing company index; adds index route; configures company factory

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   root 'welcome#index'
+  resources :companies, only: [:index]
 end
---
+++ spec/features/authenticated_user_can_see_all_companies_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.feature "User views all companies" do
+  scenario "they see picture, name, picture, number of members associated, industry, and office locations for each company" do
+    # user = create(:user)
+    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    co_1, co_2 = create_list(:company, 2)
+    visit companies_path
+
+    expect(page).to have_content(co_1.name)
+    expect(page).to have_xpath("//img[@src='#{co_1.image_url}']")
+    expect(page).to have_content(co_1.industry)
+    expect(page).to have_content(co_1.locations)
+    # number of members associated - need user table
+    expect(page).to have_content(co_2.name)
+    expect(page).to have_xpath("//img[@src='#{co_2.image_url}']")
+    expect(page).to have_content(co_2.industry)
+    expect(page).to have_content(co_2.locations)
+  end
+end
