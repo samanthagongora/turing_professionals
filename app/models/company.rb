4b797a837ff7f1420d20625cb9324575d7832b85
Ellen Cooper <ellencooper74@gmail.com>
2017-09-01 16:20:44 -0600

    adds members_through_work association

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,4 +5,6 @@ class Company < ApplicationRecord
   has_many :industries, through: :company_industries
   has_many :office_locations
   has_many :locations, through: :office_locations
+  has_many :workplaces
+  has_many :members_through_work, class_name: "User", through: :workplaces
 end
