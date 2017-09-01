279e9e2b67afee54e7353d79eaad512c3c682f1b
Katie Keel <akatiekeel@gmail.com>
2017-09-01 08:42:11 -0600

    Removed comments and unnecessary lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,54 +3,6 @@ class User < ApplicationRecord
 
   enum role: ["default", "admin"]
 
-  # belongs_to :location
-
-  # validates_presence_of :auth_token_linkedin,
-  #                       :uid,
-  #                       :first_name,
-  #                       :last_name,
-  #                       :email,
-  #                       :summary,
-  #                       :linkedin_url,
-  #                       :image_url,
-  #                       :slack,
-  #                       :github,
-  #                       :cohort
-
-  # validates_uniqueness_of :auth_token_linkedin,
-  #                         :uid,
-  #                         :email,
-  #                         :linkedin_url,
-  #                         :twitter,
-  #                         :slack,
-  #                         :github
-
-
-  # def self.find_or_create_by_auth(auth)
-    # user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid'])
-    # census_data = CensusService.find_user(auth['first_name'], auth['last_name'])
-    #
-    # return nil if census_data.nil?
-    #
-    # user.role = 0
-    # user.auth_token_linkedin = auth['access_token']
-    # user.uid = auth['info']['name']
-    # user.first_name = auth['info']['email']
-    # user.last_name = auth['uid']
-    # user.email = auth['info']['nickname']
-    # user.tagline = auth['info']['image']
-    # user.summary = auth['summary']
-    # user.linkedin_url = auth['url']
-    # user.image_url = auth['image']
-    # user.resume = nil
-    # user.location = nil
-    #
-    # user.twitter = census_data['twitter']
-    # user.slack = census_data['slack']
-    # user.github = census_data['github']
-    # user.cohort = census_data['cohort']
-    #
-    # user.save
-    # user
-  # end
+  validates_presence_of :username, :password
+  validates_uniqueness_of :username
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :sessions, only: [:new, :create, :destroy]
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
 
   resources :users, except: [:index]
 
-  get 'welcome/index'
   get '/auth/linkedin', as: :linkedin_login
   get '/auth/linkedin/callback', to: "sessions#create"
   root 'welcome#index'
