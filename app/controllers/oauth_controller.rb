d70ae4e38d81c0bf75ef46cbc31d33c462cc80ef
Katie Keel <akatiekeel@gmail.com>
2017-09-02 14:20:14 -0600

    LinkedIn profile auth appears to be working now

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -2,10 +2,10 @@ class OauthController < ApplicationController
 
   def callback
     oauth = OauthService.new(request.env['omniauth.auth'], current_user)
-
      if current_user = oauth.create_oauth_information
       redirect_to user_path(session[:user_id])
     else
+      Rails.logger.info(current_user.errors.messages.inspect)
       flash[:alert] = "There was an error"
       redirect_to root_path
     end
