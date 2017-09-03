352ef66233de4eefe3a687c011c8faa4019e13ec
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-03 15:54:21 -0600

    moving to master

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,14 @@ class User < ApplicationRecord
   has_secure_password
   validates_presence_of :username, :password
   validates_uniqueness_of :username
-<<<<<<< HEAD
 
   belongs_to :location
   has_many :workplaces
-=======
-  
+
   enum role: ["default", "admin"]
+
+  belongs_to :location, optional: true
   
-  belongs_to :location, optional: true 
   has_many :favorites
   has_many :favorite_companies, through: :favorites, source: :favoritable, source_type: 'Company'
   #returns the favoritable object
@@ -20,5 +19,4 @@ class User < ApplicationRecord
 
   mount_uploader :image_url, ImageUploader
   mount_uploader :resume, AttachmentUploader
->>>>>>> master
 end
--- db/migrate/20170902215035_change_user_location_column.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserLocationColumn < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :users, :location, :location_id
-  end
-end
