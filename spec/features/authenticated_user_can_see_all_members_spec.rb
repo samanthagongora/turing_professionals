529e7e7857fae86f67644e3710341e7e1e0526f3
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-03 17:46:41 -0600

    adds member index for active members with link instead of favorite for surrent user member card

--- spec/features/authenticated_user_can_see_all_members_spec.rb
+++ spec/features/authenticated_user_can_see_all_members_spec.rb
@@ -9,8 +9,8 @@ RSpec.feature "User visits member index" do
     click_on "Members"
 
     expect(current_path).to eq("/users")
-    expect(page).to have_css(".member-card", count: 3)
-    within first(".member-card") do
+    expect(page).to have_css(".thumbnail", count: 3)
+    within (".col-xs-6.col-sm-4.user-card-#{user_1.id}") do
       expect(page).to have_content(user_1.image_url)
       expect(page).to have_content(user_1.first_name)
       expect(page).to have_content(user_1.last_name)
@@ -22,27 +22,15 @@ RSpec.feature "User visits member index" do
       expect(page).to_not have_link('Your Profile')
     end
 
-    within second(".member-card") do
-      expect(page).to have_content(user_3.image_url)
-      expect(page).to have_content(user_3.first_name)
-      expect(page).to have_content(user_3.last_name)
-      expect(page).to have_content(user_3.location.city)
-      expect(page).to have_content(user_3.location.state)
-      expect(page).to have_content(user_3.workplaces.first.company.name)
-      expect(page).to have_content(user_3.cohort)
+    within (".user-card-#{user_2.id}") do
+      expect(page).to have_content(user_2.image_url)
+      expect(page).to have_content(user_2.first_name)
+      expect(page).to have_content(user_2.last_name)
+      expect(page).to have_content(user_2.location.city)
+      expect(page).to have_content(user_2.location.state)
+      expect(page).to have_content(user_2.workplaces.first.company.name)
+      expect(page).to have_content(user_2.cohort)
       expect(page).to have_css('.glyphicon-star-empty')
     end
-  end
-
-  xscenario "they can filter members" do
-    user_1, user_2, user_3 = create_list(:user, 3)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
-
-    click_on "Members"
-    click_on "Company"
-    click_on "City"
-    click_on "A-Z"
-    click_on "Favorites"
-    click_on "Clear Filters"
   end
 end
