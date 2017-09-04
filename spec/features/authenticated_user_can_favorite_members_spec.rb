e0ff8ce8877f268b8cf966ae6fdf3f5f68ae96cb
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-04 15:19:41 -0600

    adds test to unfavorite a member

--- spec/features/authenticated_user_can_favorite_members_spec.rb
+++ spec/features/authenticated_user_can_favorite_members_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "User visits member index", js: true do
-  scenario "can favorite a member" do
+  scenario "can favorite and unfavorite a member" do
     user_1, user_2, user_3 = create_list(:user, 3)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
@@ -13,5 +13,12 @@ RSpec.feature "User visits member index", js: true do
       expect(page).to have_css(".glyphicon-star")
     end
     expect(user_1.favorite_users).to eq([user_2])
+
+    find("body > div > div > div > div.col-xs-6.col-sm-4.user-card-#{user_2.id} > div > a > span").click
+
+    within(".user-card-#{user_2.id}") do
+      expect(page).to have_css(".glyphicon-star-empty")
+    end
+    expect(user_1.favorite_users).to eq([])
   end
 end
