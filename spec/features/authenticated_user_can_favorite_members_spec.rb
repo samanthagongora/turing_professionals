2f32d2b81affc55802b4b542fe31e18caa5e67d8
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-04 13:44:18 -0600

    adds test for filled in star for member favorite

--- spec/features/authenticated_user_can_favorite_members_spec.rb
+++ spec/features/authenticated_user_can_favorite_members_spec.rb
@@ -9,6 +9,9 @@ RSpec.feature "User visits member index", js: true do
 
     find("body > div > div > div > div.col-xs-6.col-sm-4.user-card-#{user_2.id} > div > a > span").click
 
+    within(".user-card-#{user_2.id}") do
+      expect(page).to have_css(".glyphicon-star")
+    end
     expect(user_1.favorite_users).to eq([user_2])
   end
 end
