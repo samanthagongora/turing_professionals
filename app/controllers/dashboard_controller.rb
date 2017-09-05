420a3c4933c432b4cb39988fb6ed4ada7647e25d
Desislava Todorova <dslavatodorova@gmail.com>
2017-09-04 18:54:22 -0600

    allign rows

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@ class DashboardController < ApplicationController
   before_action :require_default
 
   def show
+    @favorited_companies = current_user.favorite_companies
+    @favorite_members = current_user.favorite_users
   end
 end
