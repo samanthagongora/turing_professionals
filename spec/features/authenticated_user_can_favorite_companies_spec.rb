d3156225caf6c53275080de65c2cbf6d6d5b4e6e
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-05 15:08:51 -0600

    adds functionality to favorite a company from the index page

--- spec/features/authenticated_user_can_favorite_companies_spec.rb
+++ spec/features/authenticated_user_can_favorite_companies_spec.rb
@@ -8,19 +8,19 @@ RSpec.feature "User can favorite and unfavorite a company", js: true do
 
     visit companies_path
 
-    find("body > div > div > div.panel.panel-default.company-#{company_1.id} > div > div > div.col-md-8.text-center > h2").click
+    find("body > div > div > div.panel.panel-default.company-#{company_1.id} > div > div > div.col-md-8.text-center > h2 > a.pull-right > span").click
 
-    within("div.panel.panel-default.company-#{company_1.id}") do
+    within(".company-#{company_1.id}") do
       expect(page).to have_css(".glyphicon-star")
     end
     expect(user.favorite_companies).to eq([company_1])
 
-    find("body > div > div > div.panel.panel-default.company-#{company_1.id} > div > div > div.col-md-8.text-center > h2").click
+    find("body > div > div > div.panel.panel-default.company-#{company_1.id} > div > div > div.col-md-8.text-center > h2 > a.pull-right > span").click
 
     within("div.panel.panel-default.company-#{company_1.id}") do
       expect(page).to have_css(".glyphicon-star-empty")
     end
-    expect(user_1.reload.favorite_users).to be_empty
+    expect(user.reload.favorite_users).to be_empty
   end
 
   xscenario "when visits company show" do
