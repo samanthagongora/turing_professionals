368e95436cbc198ad05c92c4f52c4212a8815ba3
Samantha Gongora <samantha.gongora@gmail.com>
2017-09-05 15:14:58 -0600

    adds test for favoriting a company from a show page

--- spec/features/authenticated_user_can_favorite_companies_spec.rb
+++ spec/features/authenticated_user_can_favorite_companies_spec.rb
@@ -23,23 +23,23 @@ RSpec.feature "User can favorite and unfavorite a company", js: true do
     expect(user.reload.favorite_users).to be_empty
   end
 
-  xscenario "when visits company show" do
+  scenario "when visits company show" do
     company_1, company_2, company_3 = create_list(:company, 3)
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit company_path(company_2)
+    visit "/#{company_1.name.parameterize}"
 
-    find(" ").click
+    find("body > div > div.text-center.row > h1 > a.pull-right > span").click
 
-    within(" ") do
+    within("body > div > div.text-center.row > h1") do
       expect(page).to have_css(".glyphicon-star")
     end
     expect(user.favorite_companies).to eq([company_1])
 
-    find(" ").click
+    find("body > div > div.text-center.row > h1 > a.pull-right > span").click
 
-    within(" ") do
+    within("body > div > div.text-center.row > h1") do
       expect(page).to have_css(".glyphicon-star-empty")
     end
     expect(user.reload.favorite_companies).to be_empty
