37071cc10fe6fb09c3bfbba1fd81dd6521f17afc
Desislava Todorova <dslavatodorova@gmail.com>
2017-09-05 15:22:36 -0600

    add spec for dashboard

--- spec/features/user_visits_dashboard_spec.rb
+++ spec/features/user_visits_dashboard_spec.rb
@@ -1,15 +1,25 @@
-require 'rspec_helper'
+require 'rails_helper'
 
 feature "user visits dashboard" do
-  scenario "it can see its favorite users" do
+  scenario "it can see its favorite users and companies" do
     user_1, user_2, user_3 = create_list(:user, 3)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
-
     co_1, co_2 = create_list(:company, 2)
+
+    user_1.favorite_companies << co_1
+    user_1.favorite_users << user_2
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+    visit '/dashboard'
+
+    expect(page).to have_content(co_1.name)
+    expect(page).to_not have_content(co_2.name)
+    expect(page).to have_content(user_1.username)
+    expect(page).to have_content(user_2.username)
+    expect(page).to_not have_content(user_3.username)
+    expect(page).to have_css(".my-companies")
+    expect(page).to have_css(".my-connections")
+    expect(page).to have_content("My Items")
+    expect(page).to have_content("My Workplaces")
+    expect(page).to have_content("Dashboard")
   end
 end
-
-
-# When I successfully log in to our site
-# I should be redirected to my dashboard page
-# And I should see widgets for my connections, my companies, and my profile
