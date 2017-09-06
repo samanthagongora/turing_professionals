1942cff833219485ccef7d83de8e5313a83ddafb
Ellen Cooper <ellencooper74@gmail.com>
2017-09-05 21:36:42 -0600

    adds and formats highcharts placeholders

--- app/assets/javascripts/popular_technologies.js
+++ app/assets/javascripts/popular_technologies.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("turbolinks:load", function() {
   $(function () {
       var myChart = Highcharts.chart('container-pop-technologies', {
@@ -7,12 +8,30 @@ document.addEventListener("turbolinks:load", function() {
           title: {
               text: 'Most Popular Technologies Used by Members'
           },
+          plotOptions: {
+            series: {
+                shadow:false,
+                borderWidth:0,
+                dataLabels:{
+                    enabled:true,
+                    formatter:function() {
+                      return Highcharts.numberFormat(this.y,0) + '%';
+                    }
+                }
+            }
+        },
           xAxis: {
-              categories: ['Ruby', 'Javascript', 'Python']
+              categories: ['Ruby', 'Javascript', 'Python'],
           },
           yAxis: {
               title: {
                   text: '% of Members Using '
+              },
+              labels: {
+                formatter: function () {
+                  return Highcharts.numberFormat(this.value, 0) + ' %';
+                }
+
               }
           },
           series: [{
