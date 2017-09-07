b2fb18519bfac69b3cd71bd4aba9518d8ff1a15b
Ellen Cooper <ellencooper74@gmail.com>
2017-09-06 20:04:40 -0600

    adds routes and controller for workplace titles endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/technologies/most_popular', to: "technologies/technologies#index"
+      get '/workplaces/titles', to: "workplaces/titles#index"
     end
   end
 
