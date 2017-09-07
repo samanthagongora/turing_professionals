b2f893a8faa4dc5513b45462401e4db1121be2e6
Chantal Justamond <cjustamm66@me.com>
2017-09-07 13:32:41 -0600

    messages routes edited for only index and create

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   delete '/logout', to: "sessions#destroy"
 
   resources :users, except: [:destroy]
-  resources :messages
+  resources :messages, only: [:index, :create]
 
   get '/dashboard', to: "dashboard#show"
 
