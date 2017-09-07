febe7bed573d215ca6653d10045f892afc603e3b
Chantal Justamond <cjustamm66@me.com>
2017-09-06 20:28:58 -0600

    production environment config with proper Action Cable URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://turing-professionals.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://turing-professionals.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
