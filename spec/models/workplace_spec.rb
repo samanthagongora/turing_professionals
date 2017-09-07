f61a4ccff4e1619efc03ddf02d4705cb4478a28d
Ellen Cooper <ellencooper74@gmail.com>
2017-09-06 21:10:04 -0600

    adds title_counts method for workplace model; completes endpoint with passing tests

--- spec/models/workplace_spec.rb
+++ spec/models/workplace_spec.rb
@@ -7,9 +7,25 @@ RSpec.describe Workplace, type: :model do
     it {should validate_presence_of(:position)}
     it {should validate_presence_of(:status)}
   end
+
   describe "relationships" do
     it {should belong_to(:employed_user)}
     it {should belong_to(:company)}
   end
 
+  describe "class methods" do
+    it ".title_counts returns titles with count of members currently holding them" do
+      create_list(:workplace, 3, position: "Developer")
+      create_list(:workplace, 5, position: "Junior Developer")
+      create_list(:workplace, 2, position: "Senior Developer")
+      create_list(:workplace, 4, position: "Senior Developer", status: "previous")
+      create_list(:workplace, 2, position: "Software Developer", status: "previous")
+
+      expect(Workplace.title_counts.to_a.count).to eq(3)
+      expect(Workplace.title_counts.first["title"]).to eq("Junior Developer")
+      expect(Workplace.title_counts.last["title"]).to eq("Senior Developer")
+      expect(Workplace.title_counts.first["count"]).to eq(5)
+      expect(Workplace.title_counts.last["count"]).to eq(2)
+    end
+  end
 end
