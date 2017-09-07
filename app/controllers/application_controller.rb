204b3ea48f0710ff2fb7f99c7a8446b4b5b96996
Yohanan Assefa <nanahoy@gmail.com>
2017-09-07 16:43:11 -0600

    Prohibits unregistered users from accessing /locations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
   def authorize
     if !current_user
-      flash[:alert] = "You aren't authorized to visit that page."
+      flash[:alert] = "Please log in to view that page."
       redirect_to '/'
     end
   end
