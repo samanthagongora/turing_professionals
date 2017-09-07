70e33d056d56a04adc3ae2c31985cc673d13a9a4
Ellen Cooper <ellencooper74@gmail.com>
2017-09-06 22:41:30 -0600

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,13 @@ DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean
 Faker::UniqueGenerator.clear
 
+job_titles = ["Developer", "Software Engineer", "Junior Developer", "Backend Developer",
+"Senior Developer", "Programmer", "Software Architect", "Frontend Developer"]
 
-technologies = ["JavaScript", "Java", "Python", "TypeScript", "PHP", "Ruby on Rails", "Elixir", "Rust", "Go", "C#", "Swift"]
+technologies = ["JavaScript", "Java", "Python", "TypeScript", "PHP", "Ruby on Rails", "Elixir", "Rust", "Go", "C#", "Swift", "Ruby"]
 technologies.each do |tech|
   Technology.create(name: tech)
 end
-
 
 10.times do
   Industry.create(name: Faker::Job.field)
@@ -30,20 +31,22 @@ end
 end
 
 
-10.times do |n|
+50.times do |n|
   user = User.create(username: Faker::Internet.unique.user_name, password: "password", image_url: Faker::Avatar.image("50x50"), cohort: "1705 BE", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-  Workplace.create(user_id: user.id, company: Company.first, position: Faker::Job.title, status: 0)
-  user.locations << Location.all[n]
+  Workplace.create(user_id: user.id, company: Company.first, position: job_titles[rand(0..7)], status: 0)
+  user.locations << Location.all[rand(0..9)]
   4.times do
     user.technologies << Technology.all[rand(0..10)]
   end
+  user.technologies << Technology.all[11]
 end
 
-10.times do |n|
+50.times do |n|
   user = User.create(username: Faker::Internet.unique.user_name, password: "password", image_url: Faker::Avatar.image("50x50"), cohort: "1705 FE", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-  Workplace.create(user_id: user.id, company: Company.second, position: Faker::Job.title, status: 0)
-  user.locations << Location.all[n]
+  Workplace.create(user_id: user.id, company: Company.second, position: job_titles[rand(0..7)], status: 0)
+  user.locations << Location.all[rand(0..9)]
   4.times do
-    user.technologies << Technology.all[rand(0..10)]
+    user.technologies << Technology.all[rand(1..11)]
   end
+  user.technologies << Technology.all[0]
 end
