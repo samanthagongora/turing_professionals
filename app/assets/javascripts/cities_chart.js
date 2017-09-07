65d5be79a7886227e8cae8068e19f65ccc020b7f
Katie Keel <akatiekeel@gmail.com>
2017-09-07 14:41:37 -0600

    Cities chart working and chat room Javascript commented out

---
+++ app/assets/javascripts/cities_chart.js
@@ -0,0 +1,41 @@
+document.addEventListener("turbolinks:load", function(){
+
+$(document).ready(function(){
+  if($('div').is('.insights')){
+  $.ajax({
+    type: 'GET',
+    url: '/api/v1/user_locations/everywhere',
+     success: function(data) {
+       var locations = Object.keys(data)
+       var members = Object.values(data)
+
+        console.log(data, locations, members)
+        Highcharts.chart('container-cities', {
+          chart: {
+              type: 'column'
+          },
+          title: {
+              text: 'Cities Where Turing Students Have Lived After Graduation'
+          },
+          xAxis: {
+              categories: locations,
+          },
+          yAxis: {
+              min: 0,
+              title: {
+                  text: 'Number of Members'
+              }
+          },
+          legend: {
+              enabled: false
+          },
+          series: [{
+              name: 'Students',
+              data: members
+          }]
+  });
+}
+});
+}
+});
+});
